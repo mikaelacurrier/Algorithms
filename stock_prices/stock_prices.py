0e3000c2a1c09b43af58073eea66d67a1fa361b0
mikaelacurrier <mikaela.currier@gmail.com>
2019-06-05 16:27:16 -0400

    non-complete version of stock-prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,38 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  buy = sell = 0
+  profit = -1
+  change = True
+
+  for i in range(0, len(prices)-1):
+    sell = prices[i+1]
+    
+    if change:
+      buy = prices[i]
+    
+    if sell < buy:
+      change = True
+      continue
+    
+    else:
+      p = sell - buy
+      if p > profit:
+        profit = p
+      change = False
+    return sell
+  #   if p < p + 1:
+  #      buy = p
+  #      sell = p + 1
+  #      profit = buy - sell
+  #   elif p < buy:
+  #     buy = p
+  #     sell = p + 1
+  #   elif p > sell:
+  #     sell = p
+  # return profit
+
+    
 
 
 if __name__ == '__main__':
@@ -13,3 +44,5 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+  
