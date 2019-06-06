fdbfc6007c380d9a938ef4d1deda83fbb00e1ac2
mikaelacurrier <mikaela.currier@gmail.com>
2019-06-06 18:25:17 -0400

    finished stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,39 +1,21 @@
 #!/usr/bin/python
 
 import argparse
+import math
 
 def find_max_profit(prices):
-  buy = sell = 0
-  profit = -1
-  change = True
+  max_profit = -math.inf
 
-  for i in range(0, len(prices)-1):
-    sell = prices[i+1]
-    
-    if change:
-      buy = prices[i]
-    
-    if sell < buy:
-      change = True
-      continue
-    
-    else:
-      p = sell - buy
-      if p > profit:
-        profit = p
-      change = False
-    return sell
-  #   if p < p + 1:
-  #      buy = p
-  #      sell = p + 1
-  #      profit = buy - sell
-  #   elif p < buy:
-  #     buy = p
-  #     sell = p + 1
-  #   elif p > sell:
-  #     sell = p
-  # return profit
+  for idx, price in enumerate(prices):
+    future = prices[idx+1:]
+    if future:
+      max_price = max(future)
+      current = max_price - price
 
+      if current > max_profit:
+        max_profit = current
+
+  return max_profit
     
 
 
